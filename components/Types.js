982a5081cfb1cefa740c80e357f6d82e44ab1149
HuQingyang <hqy841440305@gmail.com>
2017-03-09 18:11:12 +0800

    Add api of singles.

--- components/Types.js
+++ components/Types.js
@@ -10,7 +10,10 @@ export default class Types extends React.Component {
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.style = this.style.bind(this);
         this.toggleType = this.toggleType.bind(this);
-        this.typeList = ['全部','摇滚','另类','民谣','流行','电子','古典','爵士','金属','朋克','说唱','世界音乐','氛围','原声','雷鬼','乡村','蓝调','实验','英伦','后摇','迷幻','暗潮','硬核','后朋克'];
+        this.typeList = ['全部', '摇滚', '另类', '民谣', '流行', '电子', '古典',
+            '爵士', '金属', '朋克', '说唱', '世界音乐', '氛围', '原声', '雷鬼',
+            '乡村', '蓝调', '实验', '英伦', '后摇', '迷幻', '暗潮', '华语流行',
+            '硬核', '后朋克', '人声', '品牌'];
 
         this.state = {
             type: '全部'
