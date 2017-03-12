dab7c95fa38d6609b409b4f24e7488aa3bc299bd
HuQingyang <hqy841440305@gmail.com>
2017-03-12 23:24:16 +0800

    Add traggable titlebar on macOS

---
+++ static/js/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../../components/App';
+import electron, { remote } from 'electron';
+
+
+const openExternal = electron.shell.openExternal;
+const main = remote.require('./main');
+electron.webFrame.setVisualZoomLevelLimits(1, 1);
+
+
+ReactDOM.render(
+    <div>
+        <App
+            getVolList={main.getVolList()}
+            getSingleList={main.getSingleList()}
+            openExternal={openExternal}
+        />
+    </div>,
+    document.getElementById('root')
+);
