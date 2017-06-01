b8a8b7aa75b3e745b5b49d9b12ec99f9dc2764ca
胡青杨 <hqy841440305@gmail.com>
2017-06-02 00:07:18 +0800

    Build version 1.0.0

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -1,5 +1,24 @@
 <template>
-    <img :src="'../pic/head-vol-stroked.svg'"/>
+    <div id="headBar">
+        <div id="left">
+            <div>
+                <img :src="'../pic/head-vol-solid.svg'"/>
+                <span>期刊</span>
+            </div>
+            <div>
+                <img :src="'../pic/head-track-stroked.svg'"/>
+                <span>单曲</span>
+            </div>
+        </div>
+        <div id="logo">
+            <img :src="'../pic/logo.png'"/>
+            <img :src="'../pic/logoText.svg'"/>
+        </div>
+        <div id="right">
+            <img :src="'../pic/head-track-stroked.svg'"/>
+            <span>抖腿侠</span>
+        </div>
+    </div>
 </template>
 
 
