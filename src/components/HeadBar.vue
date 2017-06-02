e32a3be91eddaf990fc4c6e99bcd730b99fd236d
胡青杨 <hqy841440305@gmail.com>
2017-06-02 13:16:04 +0800

    Building HeadBar

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -10,7 +10,7 @@
                 <p>单曲</p>
             </div>
         </div>
-        <div>
+        <div id="headBarLogo">
             <img id="headBarLogoImg" :src="'../pic/logo.png'"/>
             <img id="headBarLogoText" :src="'../pic/logoText.svg'"/>
         </div>
@@ -39,11 +39,11 @@
 
 <style lang="sass">
     #headBar
-        width: 80%
-        height: 60px
+        width: 90%
+        height: 40px
         position: fixed
-        padding: 10px 10%
-        top: 0
+        padding: 10px 5%
+        top: 10px
         display: flex
         flex-direction: row
         justify-content: space-between
@@ -52,17 +52,45 @@
             height: 100%
 
     #headBarLeft
+        height: 100%
+
         & > div
+            height: 100%
             display: inline-block
+            margin-right: 15px
+            font-size: 0.9em
+            cursor: pointer
+
+            & > img
+                height: 60%
 
     #headBarRight
+        height: 100%
+        font-size: 0.9em
+        cursor: pointer
+
         & > img
-            height: 60%
+            height: 70%
             width: auto
             border-radius: 100%
 
+    #headBarLogo
+        display: flex
+        flex-direction: row
+        align-items: center
+        cursor: pointer
+        position: relative
+        top: 8px
+
     #headBarLogoImg
-        height: 100%
+        height: 90%
         width: auto
+        margin-right: 10px
+
+    #headBarLogoText
+        height: 60%
+        width: auto
+        position: relative
+        top: 4px
 
 </style>
