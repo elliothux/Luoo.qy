04bc5fcd4c4f645da044646af1ec3c52f23c6f08
胡青杨 <hqy841440305@gmail.com>
2017-07-02 13:54:10 +0800

    Fix an issue of auto updating

--- webpack.config.js
+++ webpack.config.js
@@ -45,12 +45,8 @@ module.exports = {
             }
         ]
     },
-    // plugins: [
-    //     new webpack.DefinePlugin({
-    //         'process.env': {
-    //             NODE_ENV: '"production"'
-    //         }
-    //     })
-    // ],
+    plugins: [
+        new webpack.DefinePlugin(JSON.stringify("production"))
+    ],
     target: 'electron'
 };
