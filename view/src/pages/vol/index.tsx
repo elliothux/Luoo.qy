505628a3b25ea2685e20927bb8b2d00e67bc3f6e
HuQingyang <i@do.codes>
2019-02-02 15:33:34 +0800

    feat: mini-player

--- view/src/pages/vol/index.tsx
+++ view/src/pages/vol/index.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
-import { observer } from "mobx-react";
-import { store } from "../../store";
-import { Icon, IconTypes } from "../../components/icon";
-import { VolTrackItem } from "../../components/vol-track-item";
+import {observer} from "mobx-react";
+import {store} from "../../store";
+import {Icon, IconTypes} from "../../components/icon";
+import {VolTrackItem} from "../../components/vol-track-item";
 import "./index.scss";
 
 @observer
@@ -26,8 +26,9 @@ class Vol extends React.Component {
             ))}
           </div>
           <p id="vol-info-index">
-            vol.
-            {vol.vol}
+            vol.{vol.vol}
+              <Icon type={IconTypes.LIKE}/>
+              <Icon type={IconTypes.PLAY}/>
           </p>
           <p id="vol-info-title">{vol.title}</p>
           <div
